f867fb61c36bcd25bc1f0bfddb4b8bc2f12693e5
Thiago Fernandes <falecomobranco@yahoo.com.br>
2022-03-01 17:54:38 -0300

    Houston: a new day bring new horizons. The sun is shining up us one more time!

--- js/adicionarNovoPaciente.js
+++ js/adicionarNovoPaciente.js
@@ -1,51 +1,134 @@
-//selecionar o modal
-const modalAdd = document.querySelector('.modalAddNovo');
-//seleção de todos os campos
-const campoCoordenador = document.querySelector('#addnovo-coordenador');
-const campoNome = document.querySelector('#addNovo-nome');
-const campoEmail = document.querySelector('#addNovo-email');
-const campoCpf = document.querySelector('#addNovo-cpf');
-const campoTelefone = document.querySelector('#addNovo-telefone');
-const campoSenha = document.querySelector('#addNovo-senha');
-
-/*
-const campoSupervisorSIM = document.querySelector('#addNovo-permissaSupervisores-Sim');
-const campoSupervisorNAO = document.querySelector('#addNovo-permissaSupervisores-Nao');
-const campoEstagiarioSIM = document.querySelector('#addNovo-permissaEstagiarios-Sim').;
-const campoEstagiarioNAO = document.querySelector('#addNovo-permissaEstagiarios-Nao').value;
-const campoAnalisantesSIM = document.querySelector('#addNovo-permissaAnalisanteses-Sim').value;
-const campoAnalisantesNAO = document.querySelector('#addNovo-permissaAnalisanteses-Nao').value;
-const campoAtendimentosSIM = document.querySelector('#addNovo-permissaAtendimentoses-Sim').value;
-const campoAtendimentosNAO = document.querySelector('#addNovo-permissaAtendimentoses-Nao').value;
-*/
-
-function adicionarPaciente() {
-    
-    function montaDado(classe, campo) {
-        var nomeTd = document.createElement("td");
-        nomeTd.classList.add(classe);
-        nomeTd.textContent = campo;
-        return montaDado;
-    }
-    
-    function montaPaciente() {
-        var pacienteTr = document.createElement("tr");
-        pacienteTr.classList.add('tabela__body___tr');
-        pacienteTr.appendChild(montaDado("tabela__body___td", campoNome));
-        //montaDado("tabela__body___td", campoCurso);
-        pacienteTr.appendChild(montaDado("tabela__body___td", campoEmail));
-        pacienteTr.appendChild(montaDado("tabela__body___td", campoCpf));
-        pacienteTr.appendChild(montaDado("tabela__body___td", campoNome));
-        pacienteTr.appendChild(montaDado("tabela__body___td", campoTelefone));
-        return pacienteTr;
-    }    
-    const tabelaBody = document.querySelector('tabela.body');
-    tabelaBody.appendChild(pacienteTr);
-    return;
-}
-
 
 const botaoAdd = document.querySelector('#botao-add');
-botaoAdd.addEventListener("click", () => {
-    adicionarPaciente();
-});
+botaoAdd.addEventListener("click", function(event) {
+    event.preventDefault();
+    
+    //pego o form preenchido pelo usuario
+    var formulario = document.querySelector('#form-adiciona');
+    //coloco as informações em uma var paciente
+    var paciente = obtemPacienteDoFormulario(formulario);
+    //add o paciente na tabela
+    adicionaPacienteNaTabela(paciente);
+    
+    //fecha o modal
+    fechaModal();
+    formulario.reset();
+    
+})
+
+
+
+//----------FUNÇÔES----------//
+function obtemPacienteDoFormulario(formulario) {
+
+    //passo um formulario como parametro para a função;
+    //extraio cada info em uma variavel
+    var nomeValue = document.querySelector(".js-input-nome").value;
+    var cursoValue = document.querySelector(".js-input-curso").value;
+    var cpfValue = document.querySelector(".js-input-cpf").value;
+    var emailValue = document.querySelector(".js-input-email").value;
+    var telefoneValue = document.querySelector(".js-input-telefone").value;
+    
+    var permissoaoSupervisorValue = document.querySelector('input[name="supervisor"]:checked').value;
+    var permissoaoEstagiarioValue = document.querySelector('input[name="estagiario"]:checked').value;
+    var permissoaoAnalisantesValue = document.querySelector('input[name="analisantes"]:checked').value;
+    var permissoaoAtendimentosValue = document.querySelector('input[name="atendimentos"]:checked').value;
+
+    var permissoesValue = 
+    `Gerenciar Supervisores: ${permissoaoSupervisorValue}\n` +
+     `Gerenciar Estagiários: ${permissoaoEstagiarioValue}\n` +
+      `Gerenciar Analisantes: ${permissoaoAnalisantesValue}\n` +
+       `Gerenciar Atendimentos: ${permissoaoAtendimentosValue}`;
+
+    
+    //crio um objeto formValue preenchido com as informações do formulário
+    var formValue = {
+        nome: nomeValue,
+        curso: cursoValue,
+        cpf: cpfValue,
+        email: emailValue,
+        telefone: telefoneValue,
+        permissao: permissoesValue,
+        data: obterDataHora()
+    };
+    //disponibilizo o objeto
+    return formValue;
+}
+
+//crio uma função de montagem de dado na tabela
+function montaTd(dado, classe){
+    var td = document.createElement("td");
+    td.textContent = dado;
+    td.classList.add(classe);
+    return td;
+}
+
+//função para montagem de um paciente
+//recebe como parametro o formValue que contem os valores dos input preenchidos pelo usuario
+function montaTr(formValue){
+    var pacienteLinha = document.createElement("tr");
+    //adiciona a classe ao elemento tr
+    pacienteLinha.classList.add("tabela__body___tr");    
+    
+    //paciente linha insere dentro dele cada novo valor do paciente
+    pacienteLinha.appendChild(montaTd(formValue.nome, "tabela__body___td"));
+    pacienteLinha.appendChild(montaTd(formValue.email, "tabela__body___td"));
+    pacienteLinha.appendChild(montaTd(formValue.cpf, "tabela__body___td"));
+    pacienteLinha.appendChild(montaTd(formValue.telefone, "tabela__body___td"));
+    pacienteLinha.appendChild(montaTd(formValue.curso, "tabela__body___td"));
+    pacienteLinha.appendChild(montaTd(formValue.permissao, "tabela__body___td"));
+    pacienteLinha.appendChild(montaTd(formValue.data, "tabela__body___td"));
+    //disponibilizo o pacienteLinha
+    return pacienteLinha;
+}
+
+function adicionaPacienteNaTabela(paciente) {
+    
+    var pacienteLinha = montaTr(paciente);
+    //adiciona o paciente na tabela
+    var tabela = document.querySelector(".tabela__body");
+    tabela.appendChild(pacienteLinha);
+}
+
+function fechaModal() {
+    //selecionar o modal
+    const modal = document.querySelector('.modal-container');
+
+    //se a classe for selecionada sem erro, executa..
+    if(modal) {
+        //adicionar escuta e executa a proxima funçao
+        modal.addEventListener('click', (event) => {
+            if(event.target.id == 'botao-add') {
+                modal.classList.remove('mostrar-modal');
+            }
+        })
+    }
+}
+
+function obterDataHora() {
+    var data = new Date();
+
+    var dia = data.getDate();
+    var mes = data.getMonth();
+    var ano = data.getFullYear();    
+    var stringData = `${dia}-${mes}-${ano}`;
+
+    var hora = data.getHours();
+    var minutos = data.getMinutes();
+    var segundos = data.getSeconds();
+    var stringHora = `${hora}-${minutos}-${segundos}`;
+
+    var dataHora = `${stringData} - ${stringHora}`;
+    return dataHora;
+}
+
+//function botoesDeAcao() {
+    //criar uma tr e colocar a classe
+
+    //criar uma td i 
+    //aplicar nessa td i a classe
+
+//}
+
+   
+    
